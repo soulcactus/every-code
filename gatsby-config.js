2afb22dd1d8e14dcaa215765411fdff1911a768a
soulcactus <sunhyejin90@naver.com>
2019-09-02 18:27:03 +0900

    :gem: Feat : Add gatsby-transformer-csv

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,8 @@ module.exports = {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `images`,
-                path: `${__dirname}/src/images`,
-            },
+                path: `${__dirname}/src/images`
+            }
         },
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
@@ -22,11 +22,24 @@ module.exports = {
                 background_color: `#663399`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-            },
+                icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+            }
         },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `data`,
+                path: `${__dirname}/src/data/`
+            }
+        },
+        {
+            resolve: `gatsby-transformer-csv`,
+            options: {
+                noheader: false
+            }
+        }
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
-    ],
+    ]
 };
