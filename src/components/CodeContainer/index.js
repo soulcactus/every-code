b38408fe6f4b0c85b9f8472646d3d5e249fb125c
soulcactus <sunhyejin90@naver.com>
2019-09-04 18:50:52 +0900

    🔧 Fix :  Add propTypes

--- src/components/CodeContainer/index.js
+++ src/components/CodeContainer/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import "./index.scss";
 
-export const CodeContainer = React.memo(({ children }) => (
+const CodeContainer = React.memo(({ children }) => (
     <div className="code-container">{children}</div>
 ));
+
+CodeContainer.propTypes = {
+    children: PropTypes.node.isRequired
+};
+
+export default CodeContainer;
