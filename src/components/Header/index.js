4e3c28d088230fb7cb3c43dc1c6c99376167138f
soulcactus <sunhyejin90@naver.com>
2019-09-04 18:54:08 +0900

    🔧 Fix : Fix styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,42 +1,19 @@
-import { Link } from "gatsby";
-import PropTypes from "prop-types";
 import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "gatsby";
+
+import "./index.scss";
 
 const Header = ({ siteTitle }) => (
-    <header
-        style={{
-            background: `#5880fd`,
-            marginBottom: `1.45rem`
-        }}
-    >
-        <div
-            style={{
-                margin: `0 auto`,
-                maxWidth: 960,
-                padding: `1.45rem 1.0875rem`
-            }}
-        >
-            <h1 style={{ margin: 0 }}>
-                <Link
-                    to="/"
-                    style={{
-                        color: `white`,
-                        textDecoration: `none`
-                    }}
-                >
-                    {siteTitle}
-                </Link>
-            </h1>
+    <header>
+        <div>
+            <Link to="/">{siteTitle}</Link>
         </div>
     </header>
 );
 
 Header.propTypes = {
     siteTitle: PropTypes.string
-};
-
-Header.defaultProps = {
-    siteTitle: ``
 };
 
 export default Header;
