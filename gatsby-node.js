97c4f34f42f11b031f969b881cfa0e8d04d870dd
soulcactus <sunhyejin90@naver.com>
2019-09-10 20:14:52 +0900

    💎 Feat : Add gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,35 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const crypto = require("crypto");
+const data = require("./src/data/data.json");
 
-// You can delete this file if you're not using it
+exports.sourceNodes = ({ actions }) => {
+    const { createNode } = actions;
+
+    data.map((code) => {
+        const content = {
+            id: code.id,
+            char: code.char,
+            name: code.name,
+            catecory: code.catecory,
+            htmlEntity: code.htmlEntity,
+            htmlCode: code.htmlCode,
+            hexCode: code.hexCode,
+            cssCode: code.cssCode
+        };
+
+        const nodeContent = JSON.stringify(content);
+
+        createNode({
+            ...content,
+            parent: null,
+            children: [],
+            internal: {
+                type: `code`,
+                content: nodeContent,
+                contentDigest: crypto
+                    .createHash("md5")
+                    .update(nodeContent)
+                    .digest("hex")
+            }
+        });
+    });
+};
