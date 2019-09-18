ce648338cb1d27c7c7b02fee20016e570f712d49
soulcactus <sunhyejin90@naver.com>
2019-09-18 18:52:42 +0900

    🔧 Fix : Fix gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,13 @@
 const metaConfig = require("./gatsby-meta-config");
 const path = require("path");
+const {
+    NODE_ENV,
+    URL: NETLIFY_SITE_URL = "https://www.example.com",
+    DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+    CONTEXT: NETLIFY_ENV = NODE_ENV
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === "production";
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
 
 module.exports = {
     siteMetadata: metaConfig,
@@ -52,12 +60,22 @@ module.exports = {
             }
         },
         {
-            resolve: `gatsby-plugin-robots-txt`,
+            resolve: "gatsby-plugin-robots-txt",
             options: {
                 resolveEnv: () => NETLIFY_ENV,
                 env: {
                     production: {
-                        policy: [{ userAgent: `*` }]
+                        policy: [{ userAgent: "*" }]
+                    },
+                    "branch-deploy": {
+                        policy: [{ userAgent: "*", disallow: ["/"] }],
+                        sitemap: null,
+                        host: null
+                    },
+                    "deploy-preview": {
+                        policy: [{ userAgent: "*", disallow: ["/"] }],
+                        sitemap: null,
+                        host: null
                     }
                 }
             }
