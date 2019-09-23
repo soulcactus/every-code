ece34ccfb26c0d572bce3c06f478ad5f6271e1d3
soulcactus <sunhyejin90@naver.com>
2019-09-23 14:19:26 +0900

    💎 Feat : Add adsense

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -20,59 +20,72 @@ const detailsQuery = graphql`
 
 const Head = ({ lang, meta, title }) => {
     return (
-        <StaticQuery
-            query={detailsQuery}
-            render={(data) => {
-                const metaDescription = data.site.siteMetadata.description;
+        <>
+            <Helmet>
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                ></script>
+                <script>
+                    {`(adsbygoogle = window.adsbygoogle || []).push({
+                    google_ad_client: "ca-pub-2083709121130964",
+                    enable_page_level_ads: true});`}
+                </script>
+            </Helmet>
+            <StaticQuery
+                query={detailsQuery}
+                render={(data) => {
+                    const metaDescription = data.site.siteMetadata.description;
 
-                return (
-                    <Helmet
-                        htmlAttributes={{
-                            lang
-                        }}
-                        title={title}
-                        titleTemplate={`${data.site.siteMetadata.title}`}
-                        meta={[
-                            {
-                                name: `description`,
-                                content: metaDescription
-                            },
-                            {
-                                property: `og:title`,
-                                content: data.site.siteMetadata.title
-                            },
-                            {
-                                property: `og:image`,
-                                content: `${data.site.siteMetadata.siteUrl}${data.site.siteMetadata.image}`
-                            },
-                            {
-                                property: `og:description`,
-                                content: metaDescription
-                            },
-                            {
-                                property: `og:type`,
-                                content: `website`
-                            },
-                            {
-                                name: `google-site-verification`,
-                                content: `CuSkWObnLIgbk3n4J-eSJiQ6w1pt-MtfZYo3s2dOHaI`
-                            }
-                        ]
-                            .concat(
-                                data.site.siteMetadata.keywords.length > 0
-                                    ? {
-                                          name: `keywords`,
-                                          content: data.site.siteMetadata.keywords.join(
-                                              `, `
-                                          )
-                                      }
-                                    : []
-                            )
-                            .concat(meta)}
-                    />
-                );
-            }}
-        />
+                    return (
+                        <Helmet
+                            htmlAttributes={{
+                                lang
+                            }}
+                            title={title}
+                            titleTemplate={`${data.site.siteMetadata.title}`}
+                            meta={[
+                                {
+                                    name: `description`,
+                                    content: metaDescription
+                                },
+                                {
+                                    property: `og:title`,
+                                    content: data.site.siteMetadata.title
+                                },
+                                {
+                                    property: `og:image`,
+                                    content: `${data.site.siteMetadata.siteUrl}${data.site.siteMetadata.image}`
+                                },
+                                {
+                                    property: `og:description`,
+                                    content: metaDescription
+                                },
+                                {
+                                    property: `og:type`,
+                                    content: `website`
+                                },
+                                {
+                                    name: `google-site-verification`,
+                                    content: `CuSkWObnLIgbk3n4J-eSJiQ6w1pt-MtfZYo3s2dOHaI`
+                                }
+                            ]
+                                .concat(
+                                    data.site.siteMetadata.keywords.length > 0
+                                        ? {
+                                              name: `keywords`,
+                                              content: data.site.siteMetadata.keywords.join(
+                                                  `, `
+                                              )
+                                          }
+                                        : []
+                                )
+                                .concat(meta)}
+                        />
+                    );
+                }}
+            />
+        </>
     );
 };
 
